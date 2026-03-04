38a17e87257643b55deaced952e9a71c39a93a17
Ori <ori.rowan@gmail.com>
2026-03-04 14:52:35 +0100

    EventManager docs

--- src/event_manager/event_manager.p8.lua
+++ src/event_manager/event_manager.p8.lua
@@ -1,17 +1,29 @@
 -- priority: 1
 -- tokens: 102
 
---- Global manager for event system
--- @classmod EventManager
--- @field events table Internal queue for pending events.
--- @field listeners table A list of active listeners.
+--- Global singleton manager for the event system. Listeners and events do not have a 
+-- deterministic order. Do not rely on multiple events/listeners triggering in a 
+-- particular sequence. Event props are shared across listeners, so if a handler mutates 
+-- props, subsequent listeners will see the changes.
+-- @type EventManager
+-- @field name
+-- @field health
+    
 EventManager = {
-    events = {},
-    listeners = {}
+    --- Internal queue for pending events. Events are tables like this:
+    -- @field 1 (string) event
+    -- @field 2 (table) props
+    events = {}, 
+    --- A list of active listeners. Listeners are tables like this: 
+    -- @field 1 (string) event
+    -- @field 2 (function) func
+    -- @field 3 (bool) die
+    listeners = {} 
 }
 
 
---- Updates the EventManager.
+--- Update function for EventManager.
+-- Processes all events by calling listener functions. After processing, the event queue is cleared.
 function EventManager:update()
     foreach(self.listeners, function (l)
         foreach(self.events, function (e)
@@ -25,23 +37,24 @@ function EventManager:update()
 end
 
 --- Adds a new listener tied to an event.
--- @param event string Name of event to listen to.
--- @param func function Handler function. Should have one param table props.
--- @param die boolean If true then listener dies after one trigger.
+-- @tparam string event Name of event to listen to.
+-- @tparam function func Handler function that recieves one parameter: props table.
+-- @tparam bool die If true, the listener is removed after one trigger.
 function EventManager:add_listener(event, func, die)
     add(self.listeners,{event,func,die}) -- 1=event, 2=func, 3=die
 end
 
 --- Emits an event with props.
--- @param event string Name of the event emitted.
--- @param props table Props for handler function.
--- @param reactive boolean If true the props will be passed directly otherwise props are passed via safe non-reactive copy.
+-- @tparam string event Name of the event emitted.
+-- @tparam table props Props for handler function.
+-- @tparam bool reactive If true, props are passed directly; otherwise a safe copy is used.
 function EventManager:emit(event, props, reactive)
     add(self.events, {event, reactive and props or copy_table(props)}) -- 1=event, 2=props
 end
 
 --- Resets EventManager state.
 -- Clears all listeners and event queue.
+-- @function EventManager:clear
 function EventManager.clear(_ENV)
     events, listeners  = {}, {}
 end
