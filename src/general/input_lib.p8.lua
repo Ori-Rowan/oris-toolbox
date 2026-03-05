23f8dbf7612cdf2caf4e5eb8cfc3eb1ecd7d1fa5
Ori <ori.rowan@gmail.com>
2026-03-05 11:25:32 +0100

    input_lib docs fix

--- src/general/input_lib.p8.lua
+++ src/general/input_lib.p8.lua
@@ -7,7 +7,7 @@
 
 
 --- Get direction based on the player input ⬅️⬆️➡️⬇️ for normalized 8d input.
--- @treturn ?angle Combined input direction or empty on no input or colliding input (left+right, up+down).
+-- @treturn angle|empty Combined input direction or empty on no input or colliding input (left+right, up+down).
 function get_8d_input()
     return tonum(split"0.5,0,,0.25,0.375,0.125,0.25,0.75,0.625,0.875,0.75,,0.5,0"[btn()&0b1111])
 end
