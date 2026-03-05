70adbce8b9b64ce7ce0e3dde0ac1adc707e55936
Ori <ori.rowan@gmail.com>
2026-03-05 15:40:02 +0100

    entity_behaviour fix

--- src/general/entity_behaviour.p8.lua
+++ src/general/entity_behaviour.p8.lua
@@ -1,5 +1,5 @@
 -- priority: -1
--- tokens:24
+-- tokens:28
 
 --- Entity behaviour.
 -- Helper function for entities in a 2D space.
@@ -10,7 +10,7 @@
 -- @tparam number e.spd Number of pixels to move.
 -- @tparam angle e.dir Movement direction.
 function entity_movement(e)
-  e.x+=cos(e.dir)*e.spd
-  e.y+=sin(e.dir)*e.spd
+  if(e.dir) e.x+=cos(e.dir)*e.spd e.y+=sin(e.dir)*e.spd
 end
+
 
