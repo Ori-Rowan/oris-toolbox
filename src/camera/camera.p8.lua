1bdb48003c5fd3f85f27e622201522df729013c3
Ori <ori.rowan@gmail.com>
2026-03-05 11:21:10 +0100

    camera docs

--- src/camera/camera.p8.lua
+++ src/camera/camera.p8.lua
@@ -1,18 +1,27 @@
 -- priority: 1
 
+--- Global camera object to handle camera position.
+-- @type Camera
+
 Camera = {
+    --- (number) Maximum offset for camera shake.
     shake_offset = 2,
+    --- (number) Internal timer for camera shake.
     shake_timer = 0,
 }
 
-function Camera:shake(frames)
-    self.shake_timer=frames
+--- Begin camera shaking for amount of frames.
+-- @tparam number f Amount of frames to shake for.
+function Camera:shake(f)
+    self.shake_timer=f
 end
 
+--- Update step function.
 function Camera:update()
     if (self.shake_timer > 0) self.shake_timer-=1 
 end
 
+--- Draw step function.
 function Camera:draw()
     if self.shake_timer > 0 then
         camera(self:get_offset(), self:get_offset())
@@ -21,6 +30,9 @@ function Camera:draw()
     end
 end
 
+--- Get random camera offset.
+-- offset is calculated based on Camera.shake_offset.
+-- @return number offset
 function Camera:get_offset()
     return rnd(self.shake_offset*2) - self.shake_offset
 end
